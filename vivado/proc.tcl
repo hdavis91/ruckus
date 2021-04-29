a06a0ff26360f0f96ab88438937a1c9b6d0b92a4
Larry Ruckman <ruckman@slac.stanford.edu>
2021-04-29 14:11:25 -0700

    moving build string generation from sources.tcl to proc.tcl

--- vivado/proc.tcl
+++ vivado/proc.tcl
@@ -331,6 +331,50 @@ proc GenerateBdWrappers { } {
 
 }
 
+## Generate the build string
+proc GenerateBuildString {} {
+
+   # Generate the build string
+   binary scan [encoding convertto ascii $::env(BUILD_STRING)] c* bstrAsic
+   set buildString ""
+   foreach decChar ${bstrAsic} {
+      set hexChar [format %02X ${decChar}]
+      set buildString ${buildString}${hexChar}
+   }
+   for {set n [string bytelength ${buildString}]} {$n < 512} {incr n} {
+      set padding "0"
+      set buildString ${buildString}${padding}
+   }
+
+   # Generate the Firmware Version string
+   scan ${PRJ_VERSION} %x decVer
+   set fwVersion [format %08X ${decVer}]
+
+   # Generate the GIT SHA-1 string
+   set gitHash $::env(GIT_HASH_LONG)
+   while { [string bytelength $gitHash] != 40 } {
+      set gitHash "0${gitHash}"
+   }
+
+   # Set the top-level generic values
+   set buildInfo "BUILD_INFO_G=2240'h${gitHash}${fwVersion}${buildString}"
+   set_property generic ${buildInfo} -objects [current_fileset]
+
+   # Auto-generate a "BUILD_INFO_C" VHDL package for Dynamic Partial Reconfiguration builds
+   set pathToPkg "${OUT_DIR}/${VIVADO_PROJECT}.srcs/BuildInfoPkg.vhd"
+   exec mkdir -p ${OUT_DIR}/${VIVADO_PROJECT}.srcs
+   set out [open ${pathToPkg} w]
+   puts ${out} "library ieee;"
+   puts ${out} "use ieee.std_logic_1164.all;"
+   puts ${out} "library surf;"
+   puts ${out} "use surf.StdRtlPkg.all;"
+   puts ${out} "package BuildInfoPkg is"
+   puts ${out} "constant BUILD_INFO_C : BuildInfoType :=x\"${gitHash}${fwVersion}${buildString}\";"
+   puts ${out} "end BuildInfoPkg;"
+   close ${out}
+
+}
+
 ## Generate Verilog simulation models for a specific .dcp file
 proc DcpToVerilogSim {dcpName} {
    source -quiet $::env(RUCKUS_DIR)/vivado/env_var.tcl
